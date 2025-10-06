b3c8b23ee1bce9e6767a6e92cc1d80485674f12f
mookie34 <santiago.mazo34@gmail.com>
2025-10-06 18:41:44 -0500

    add sidebar

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,14 +1,19 @@
-import { Component, signal } from '@angular/core';
+import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { Navbar} from './shared/components/navbar/navbar';
+import { SidebarComponent } from './layout/sidebar/sidebar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, Navbar],
-  templateUrl: './app.html',
-  styleUrl: './app.css'
+  imports: [RouterOutlet, SidebarComponent],
+  template: `
+    <div class="flex min-h-screen bg-gray-50">
+      <app-sidebar></app-sidebar>
+
+      <main class="flex-1 p-6">
+        <router-outlet></router-outlet>
+      </main>
+    </div>
+  `
 })
-export class App {
-  protected readonly title = signal('YouNowApp');
-}
+export class App {}
---
+++ src/app/layout/sidebar/sidebar.component.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
+import { NgFor } from '@angular/common';
+
+@Component({
+  selector: 'app-sidebar',
+  standalone: true,
+  imports: [RouterLink, RouterLinkActive, NgFor],
+  templateUrl: './sidebar.component.html'
+})
+export class SidebarComponent {
+  menu = [
+    { label: 'Dashboard', icon: '🏠', route: '/dashboard', exact: true },
+    {label: 'Clientes', icon: '👥', route: '/usuarios' },
+    { label: 'Productos', icon: '🛒', route: '/productos' },
+    { label: 'Pedidos', icon: '📦', route: '/pedidos' },
+    { label: 'Validar pagos', icon: '✅', route: '/validar-pagos' },
+    { label: 'Domiciliarios', icon: '🚴‍♂️', route: '/domiciliarios' },
+    { label: 'Conciliación', icon: '💲', route: '/conciliacion' },
+    { label: 'Configuración', icon: '⚙️', route: '/configuracion' },
+  ];
+}

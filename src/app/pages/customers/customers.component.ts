5d40c3d4cac8f6447fe8113c627347f8a6bb22ca
mookie34 <santiago.mazo34@gmail.com>
2025-10-07 09:23:11 -0500

    Se agrega botón para agregar un nuevo cliente.

--- src/app/pages/customers/customers.component.ts
+++ src/app/pages/customers/customers.component.ts
@@ -1,17 +1,26 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 import { CustomerService, Customer } from '../../services/customer';
 
 @Component({
   selector: 'app-customers',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   templateUrl:'./customers.component.html',
 })
 export class CustomersComponent implements OnInit {
   customers: Customer[] = [];
   loading = true;
   error = '';
+
+  //-- Variables para el formulario de creación de clientes --
+  showForm = false;
+  customerForm = {
+    name:'',
+    email:'',
+    phone:''
+  };
 
   constructor(private customerService: CustomerService) { }
 
@@ -35,6 +44,35 @@ export class CustomersComponent implements OnInit {
       },
     });
   }
+
+  //-- Abrir modal de formulario --
+  openForm(): void {
+    this.customerForm = { name: '', email: '', phone: '' };
+    this.showForm = true;
+  };
+  //-- Cerrar modal de formulario --
+  closeForm(): void {
+    this.showForm = false;
+  };
+
+  //-- Guardar nuevo cliente --
+  saveCustomer(): void {
+    if(!this.customerForm.name || !this.customerForm.phone) {
+      alert('El nombre y teléfono son obligatorios.');
+      return;
+    }
+
+    this.customerService.createCustomer(this.customerForm).subscribe({
+      next: (newCustomer) => {
+        this.loadCustomers(); // recargar lista
+        this.closeForm(); // cerrar modal
+      },
+      error: (err) => {
+        console.error('Error al crear cliente:', err);
+        alert('No se pudo crear el cliente.');
+      }
+    });
+  };
 
 }
 
